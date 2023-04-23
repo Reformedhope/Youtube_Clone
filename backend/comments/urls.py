094e591c8c9fbd82e9fa9bbd286ebfec4cc35d61
Stephanie Salzburg <steph_salzburg@hotmail.com>
2023-04-23 18:10:49 -0400

    updated urls in the project folder

--- backend/comments/urls.py
+++ backend/comments/urls.py
@@ -1,7 +1,9 @@
-import django.urls import path 
+from django.urls import path  
 from . import views
 
 urlpatterns = [
-    path('', views.users_comments),
-    path('<video_id>', views.get_all_comments),
+    path('', views.user_comments),
+    path('<int:video_id>', views.get_all_comments),
+    
+
 ]
