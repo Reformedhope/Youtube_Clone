2856d4319ae3b2252bd9f533377d9f431ed8f6a3
Stephanie Salzburg <steph_salzburg@hotmail.com>
2023-07-11 21:37:22 -0400

    added a get request to pull replies

--- backend/replies/views.py
+++ backend/replies/views.py
@@ -5,6 +5,17 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.decorators import api_view, permission_classes
 from .models import Reply
 from .serializers import ReplySerializer
+from rest_framework import status
 # Create your views here.
+
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def  get_all_replies(request, video_id):  # What goes into the () is the parameters that can be used in the function. 
+    reply_comments = Reply.objects.filter(video_id=video_id)
+    serializer = ReplySerializer(reply_comments, many=True)
+    return Response(serializer.data)
+
+
 
 
