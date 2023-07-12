0dbe57ad418e06dfbdaf3f0f5dc40ce61d4b2848
Stephanie Salzburg <steph_salzburg@hotmail.com>
2023-07-12 19:37:54 -0400

    updated the views file to have protected post request

--- backend/replies/views.py
+++ backend/replies/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
@@ -15,6 +15,20 @@ def  get_all_replies(request, video_id):  # What goes into the () is the paramet
     reply_comments = Reply.objects.filter(video_id=video_id)
     serializer = ReplySerializer(reply_comments, many=True)
     return Response(serializer.data)
+
+
+@api_view(['GET','POST'])
+@permission_classes([IsAuthenticated])
+def user_video_repies(request,video_id):
+    print(
+        'User',f" {request.user.id}{request.user.email} {request.user.username}")
+    if request.method == 'POST':
+        serializer = ReplySerializer(data =request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user, video_id=video_id)
+            return Response (serializer.data, status=status.HTTP_201_CREATED)
+        return Response (serializer.errors, status.HTTP_400_BAD_REQUEST)
+    
 
 
 
